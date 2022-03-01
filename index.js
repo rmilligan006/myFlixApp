3dcd6f007377435a25cf9f3f5824ab6d1526bd7a
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-03-01 12:56:28 -0500

    bunch of changes to the index, however it is not complete yet.

--- index.js
+++ index.js
@@ -3,51 +3,97 @@ const express = require('express'),
 
 const app = express();
 const bodyParser = require('body-parser'),
+  uuid = require('uuid');
   methodOverride = require('method-override');
+const { rest, bindAll } = require('lodash');
+
+
+let users = [
+  {
+    id: 1,
+    name: "Bill",
+    favoriteMovies: []
+  },
+  {
+    id: 2,
+    name: "Bruce",
+    favoriteMovies: ["Blues Brothers"]
+  }
+]
 
   let topMovies = [ // A small list of favourite movies
     {
-      title: 'Blues Brothers',
-      Director: 'John Landis'
+      title: "Blues Brothers",
+      Director: {
+        Name: "John Landis"
+      },
+      Genre: {
+        Name: "comedy",
+        description: "Movies to make you laugh and feel happy"
+      }
     },
     {
-      title: 'The Godfather',
-      director: 'Francis Ford Coppola'
+      title: "The Godfather",
+      director: "Francis Ford Coppola",
+      genre:"Crime-drama"
     },
     {
-      title: 'The Departed',
-      Director: 'Martin Scorsese'
+      title: "The Departed",
+      Director: "Martin Scorsese",
+      genre:"Crime-drama"
     },
     {
-      title: 'Rush Hour 2',
-      director: 'Brett Ratner'
+      title: "Rush Hour 2",
+      director: "Brett Ratner",
+      genre:"Comedy"
     },
     {
-      title: 'Gran Torino',
-      director: 'Clint Eastwood'
+      title: "Gran Torino",
+      director: "Clint Eastwood",
+      genre:"drama-thriller"
     },
     {
-      Title: 'John Wick',
-      Director: 'Chad Stahelski'
+      title: "John Wick",
+      Director: "Chad Stahelski",
+      genre:"Action"
     },
     {
-      Title: '300',
-      Director: 'Zack Snyder'
+      title: "300",
+      Director: "Zack Snyder",
+      genre:"Action"
     },
     {
-      Title: 'Slap Shot',
-      Director: 'Geroge Roy Hill'
+      title: "Slap Shot",
+      Director: "Geroge Roy Hill",
+      genre:"Comedy"
+
     },
     {
-      Title: 'Goon',
-      Director: 'Michael Dowse'
+      title: "Goon",
+      Director: "Michael Dowse",
+      genre:"Comedy"
     },
     {
-      Title: 'The Rocket',
-      Director: 'Charles Biname'
+      title: "The Rocket",
+      Director: "Charles Biname",
+      genre:"drama"
     }
 
   ];
+
+// Create!
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('users need names')
+  }
+})
+
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -74,8 +120,46 @@ app.get('/documentation', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.status(200).json(topMovies);
 });
+
+//READ!!!
+app.get('/movies/:title', (req, res) => {
+    const  { title } = req.params;
+    const movie = topMovies.find(movie => movie.title === title );
+
+    if (movie) {
+      res.status(200).json(movie);
+    } else {
+      res.status(400).send('No such movie!')
+    }
+
+});
+
+app.get('/movies/genre/:genreName', (req, res) => {
+  const  { genreName } = req.params;
+  const genre = topMovies.find(movie => movie.Genre.Name === genreName ).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('No such genre!')
+  }
+
+});
+
+app.get('/movies/director/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = topMovies.find( movie => movie.Director.Name === directorName).Director;
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('no such director!')
+  }
+})
+
+
+
 
 
 //Listen Commands!
