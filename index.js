3854e6eff41ea1b6df1727f6bc6e9c9cb974e7f0
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-03-09 13:22:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const { check, validationResult } = require('express-validator');
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb://process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //USE, GET, POST, DELETE methods
 app.use(bodyParser.urlencoded({
