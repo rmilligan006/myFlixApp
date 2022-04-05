b8b7a993c40b1c7c76bec635b1019ad3a9aca785
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-04-05 14:13:37 -0400

    changes....

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ app.delete(
         if (!user) {
           res.status(400).send(req.params.Username + " was not found");
         } else {
-          res.status(204).end;
+          res.status(200).send(req.params.Username + " was deleted.");
         }
       })
       .catch((err) => {
